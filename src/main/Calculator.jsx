4b7dae5bb563b9e5868abbf031ccc77b2de70412
viannaandreBR <vianna_andre@hotmail.com@users.noreply.github.com>
2020-07-10 05:29:03 -0300

    Commit Logic Implementation #02

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -27,8 +27,32 @@ export default class Calculator extends Component{
     this.setState({...initialState})
   }
 
-  setOperation(operation) {
-    console.log(operation)
+  setOperation(operation) { 
+    if(this.state.current === 0){
+      this.setState({operation, current: 1, clearDisplay: true})
+    } else {
+      const equals = operation === '='
+      const currentOperation = this.state.operation
+
+      const values = [...this.state.values]
+      
+      try {
+          values[0]= eval(`${values[0]} ${currentOperation} ${values[1]}`)   
+      } catch(e) {
+        values[0] = this.setState.values[0]
+      }
+
+
+      values[1] = 0
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values
+      })
+    }
   }
 
   addDigit(n){
